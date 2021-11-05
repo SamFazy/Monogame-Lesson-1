6c4520e5e0ba0acf2086041f2dbc73f25db4985e
Sam F <samfaz10@hotmail.com>
2021-11-05 13:17:27 -0400

    Add project files.

---
+++ Monogame Lession 1/Game1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Lession_1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D dinoTexture;
+        Texture2D rockTexture;
+
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            // Make The Window 800 x 500
+            _graphics.PreferredBackBufferWidth = 1000;
+            _graphics.PreferredBackBufferHeight = 1000;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "My First Monogame Project";
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            dinoTexture = Content.Load<Texture2D>("Dino");
+            rockTexture = Content.Load<Texture2D>("Rock 1");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Blue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(rockTexture, new Vector2(100, 100), Color.White);
+            _spriteBatch.Draw(dinoTexture, new Vector2(550, 70), Color.White);
+            _spriteBatch.Draw(dinoTexture, new Vector2(300, 50), Color.White);
+
+           
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
